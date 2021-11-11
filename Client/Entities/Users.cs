9138136dfb888d77438aa13054e8a2a97b44a96a
Shihab Ahmed Efty <shihabahmed620@gmail.com>
2021-11-12 01:10:03 +0600

    [Data Access]

---
+++ Client/Entities/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital_Management_System.Client.Entities
+{
+    class Users
+    {
+        public enum UserType
+        {
+            Admin,
+            Doctor,
+            Nurse
+        };
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Users(int id, string userName, string password)
+        {
+            Id = id;
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
---
+++ Database/DataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Hospital_Management_System.Database
+{
+    class DataAccess
+    {
+        
+        public string ConnectionString { get; set; }
+        public DataAccess()
+        {
+            ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=G:\AIUB\HMS\db\hospital.mdf;Integrated Security=True;Connect Timeout=30";
+        }
+        public SqlCommand GetComand(string sqlQuery)
+        {
+
+        }
+    }
+}
